31b04b440b21fb915856e79ec1cd8d3bcc1f9927
mdhttr <etoledano9@gmail.com>
2019-02-19 23:30:32 +0200

    -added firebase support -fixed the new activity transitions -added list names -added unique user id

---
+++ app/src/main/java/com/example/moriyaamar/project/FragNewListNameDialog.java
@@ -0,0 +1,81 @@
+package com.example.moriyaamar.project;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class FragNewListNameDialog extends DialogFragment {
+
+    private ListNameListener listNameListener;
+    private EditText listNameEditText;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof FragNewListNameDialog.ListNameListener)
+            listNameListener=(FragNewListNameDialog.ListNameListener)context;
+        else
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+       // return super.onCreateDialog(savedInstanceState);
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity()).setTitle("List Name").setMessage("Set new list name")
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        if(!(listNameEditText.getText().toString()).equals("")) {
+                            listNameListener.onListNameApproved(listNameEditText.getText().toString());
+                        }
+                        else {
+                            Date currentDate = Calendar.getInstance().getTime();
+                            listNameListener.onListNameApproved(currentDate.toString().substring(0,19));
+                        }
+                    }
+                }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dismiss();
+                    }
+                });
+
+        View view=getActivity().getLayoutInflater().inflate(R.layout.frag_insert_list__name,null);
+        builder.setView(view);
+
+        listNameEditText = (EditText)view.findViewById(R.id.nameEditText);
+
+        return builder.create();
+    }
+
+    public interface ListNameListener{
+        public void onListNameApproved(String name);
+    }
+
+
+}
--- app/src/main/java/com/example/moriyaamar/project/MainActivity.java
+++ app/src/main/java/com/example/moriyaamar/project/MainActivity.java
@@ -1,17 +1,63 @@
 package com.example.moriyaamar.project;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, FragNewListNameDialog.ListNameListener {
     private Button newListBtn, editListBtn;
+    private String newListName;
+    private String uniqueId;
+    private final int MY_PERMISSIONS_REQUEST_READ_PHONE_STATE=1;
+    private DatabaseReference appDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        appDatabase = FirebaseDatabase.getInstance().getReference();
+
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_PHONE_STATE) == PackageManager.PERMISSION_GRANTED){
+            Context AppContext = getApplicationContext();
+            TelephonyManager tMgr = (TelephonyManager) AppContext.getSystemService(Context.TELEPHONY_SERVICE);
+            uniqueId = tMgr.getDeviceId();
+        }
+        else
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_PHONE_STATE}, MY_PERMISSIONS_REQUEST_READ_PHONE_STATE);
+
+        appDatabase.child("users").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists()){
+                    //User already exits
+                } else {
+                    appDatabase.child("users").child(uniqueId).setValue("Grinch");      //Change Grinch to username requested from user
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
         newListBtn = (Button)findViewById(R.id.addListBtn);
         editListBtn = (Button)findViewById(R.id.editListBtn);
@@ -19,7 +65,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         newListBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                setContentView(R.layout.new_list_activity);        /*Load new list activity here*/
+
+                new FragNewListNameDialog().show(getSupportFragmentManager(),null);
             }
         });
 
@@ -29,7 +76,34 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-        //setContentView(R.Layout. )                /*Load edit list activity here*/
+        Intent myIntent = new Intent(MainActivity.this, EditListActivity.class);
+        MainActivity.this.startActivity(myIntent);              /*Load edit list activity here*/
     }
 
+    @Override
+    public void onListNameApproved(String name) {
+        this.newListName = name;
+
+        ///////////////////////////////////////////////////////////////////////////////////////
+        //////////////////////ENTER NEW LIST TO DATABASE HERE//////////////////////////////////
+        ///////////////////////////////////////////////////////////////////////////////////////
+
+        Intent myIntent = new Intent(MainActivity.this, NewListActivity.class);
+        myIntent.putExtra("listName", newListName);       //pass ListName
+        MainActivity.this.startActivity(myIntent);              /*Load new list activity here*/
+    }
+
+    /**Check permission in order to obtain unique user ID*/
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case MY_PERMISSIONS_REQUEST_READ_PHONE_STATE:
+                if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_PHONE_STATE) == PackageManager.PERMISSION_GRANTED) {
+                    Context AppContext = getApplicationContext();
+                    TelephonyManager tMgr = (TelephonyManager)AppContext.getSystemService(Context.TELEPHONY_SERVICE);
+                    uniqueId = tMgr.getDeviceId();
+                }
+        }
+    }
 }
