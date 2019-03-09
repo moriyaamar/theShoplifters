42721ff8cac25bde420d4f0615d804b3ea2818b1
mdhttr <etoledano9@gmail.com>
2019-03-09 23:37:51 +0200

    -deleted unnecessary imports

--- app/src/main/java/com/example/moriyaamar/project/NewListActivity.java
+++ app/src/main/java/com/example/moriyaamar/project/NewListActivity.java
@@ -7,18 +7,13 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
-
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
