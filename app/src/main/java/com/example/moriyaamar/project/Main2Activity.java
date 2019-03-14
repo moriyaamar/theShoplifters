37c916ec0d7eba74396159cc1e1fa16214efa257
mdhttr <etoledano9@gmail.com>
2019-03-14 23:16:01 +0200

    -permissions bug fix (if no uid found)

--- app/src/main/java/com/example/moriyaamar/project/Main2Activity.java
+++ app/src/main/java/com/example/moriyaamar/project/Main2Activity.java
@@ -39,7 +39,7 @@ public class Main2Activity extends AppCompatActivity implements View.OnClickList
     private SmsBroadcastReceiver smsReceiver;
     private IntentFilter smsFilter;
     private final String FRAG_ABOUT="FRAG_ABOUT";
-    private boolean clicked=false;
+    private boolean clicked=false, permissions=false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,21 +47,20 @@ public class Main2Activity extends AppCompatActivity implements View.OnClickList
         setContentView(R.layout.activity_main2);
 
         TextView learnTextView = findViewById(R.id.learnTextView);
-        learnTextView.setPaintFlags(learnTextView.getPaintFlags() |   Paint.UNDERLINE_TEXT_FLAG);
+        learnTextView.setPaintFlags(learnTextView.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
 
         learnTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                if(!clicked) {
+                if (!clicked) {
                     FragAbout fragAbout = new FragAbout();
                     FragmentTransaction fragTrans = getSupportFragmentManager().beginTransaction().
                             add(R.id.aboutFrameLayoutContainer, fragAbout, FRAG_ABOUT).addToBackStack(null);
                     fragTrans.commit();
-                    clicked=true;
-                }
-                else{
-                    clicked=false;
+                    clicked = true;
+                } else {
+                    clicked = false;
                     FragAbout fragAbout = new FragAbout();
                     FragmentTransaction fragTrans = getSupportFragmentManager().beginTransaction();
                     fragTrans.hide(getSupportFragmentManager().findFragmentByTag(FRAG_ABOUT));
@@ -71,8 +70,8 @@ public class Main2Activity extends AppCompatActivity implements View.OnClickList
         });
 
         /*Show application start for 3 seconds*/
-        newListBtn = (Button)findViewById(R.id.addListBtn);
-        editListBtn = (Button)findViewById(R.id.editListBtn);
+        newListBtn = (Button) findViewById(R.id.addListBtn);
+        editListBtn = (Button) findViewById(R.id.editListBtn);
 
 
         smsFilter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);              //Prepare IntentFilter for the SMS BroadcastReceiver
@@ -81,24 +80,61 @@ public class Main2Activity extends AppCompatActivity implements View.OnClickList
 
         appDatabase = FirebaseDatabase.getInstance().getReference();
 
-        if (ContextCompat.checkSelfPermission(Main2Activity.this, Manifest.permission.READ_PHONE_STATE) == PackageManager.PERMISSION_GRANTED){       //Check app permissions
-            Context AppContext = getApplicationContext();                                                                                                   //
+        if (ContextCompat.checkSelfPermission(Main2Activity.this, Manifest.permission.READ_PHONE_STATE) == PackageManager.PERMISSION_GRANTED) {       //Check app permissions
+            Context AppContext = getApplicationContext();//
+            permissions=true;
             TelephonyManager tMgr = (TelephonyManager) AppContext.getSystemService(Context.TELEPHONY_SERVICE);                                              //
             uniqueId = tMgr.getDeviceId();
             smsReceiver = new SmsBroadcastReceiver(uniqueId);                                           //Creating new BroadcastReceiver
-        }
-        else
+        } else
             ActivityCompat.requestPermissions(Main2Activity.this, new String[]{Manifest.permission.READ_PHONE_STATE}, MY_PERMISSIONS_REQUEST_READ_PHONE_STATE);      //No permissions found, request permissions
 
-        if(ContextCompat.checkSelfPermission(Main2Activity.this, Manifest.permission.READ_SMS)!=PackageManager.PERMISSION_DENIED)                     //Check other app permissions
+ /*       if (ContextCompat.checkSelfPermission(Main2Activity.this, Manifest.permission.READ_SMS) != PackageManager.PERMISSION_DENIED)                     //Check other app permissions
+            this.registerReceiver(smsReceiver, smsFilter);                                                                                                   //Register the BroadcastReceiver if permissions granted
+        else
+            ActivityCompat.requestPermissions(Main2Activity.this, new String[]{Manifest.permission.READ_SMS}, MY_PERMISSIONS_REQUEST_READ_SMS);     */   //No permissions found, request permissions
+
+        if(permissions)
+            startApp();
+          /*  appDatabase.child("users").child(uniqueId).addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    if (dataSnapshot.exists()) {
+                        //User already exits
+                    } else {
+                        appDatabase.child("users").child(uniqueId).setValue("Grinch");      //Change Grinch to username requested from user - Optional, not critical
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            });
+
+            newListBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    new FragNewListNameDialog().show(getSupportFragmentManager(), null);
+                }
+            });
+
+            editListBtn.setOnClickListener(this);*/
+        }
+
+    private void startApp() {
+
+        if (ContextCompat.checkSelfPermission(Main2Activity.this, Manifest.permission.READ_SMS) != PackageManager.PERMISSION_DENIED)                     //Check other app permissions
             this.registerReceiver(smsReceiver, smsFilter);                                                                                                   //Register the BroadcastReceiver if permissions granted
         else
             ActivityCompat.requestPermissions(Main2Activity.this, new String[]{Manifest.permission.READ_SMS}, MY_PERMISSIONS_REQUEST_READ_SMS);        //No permissions found, request permissions
 
+
         appDatabase.child("users").child(uniqueId).addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                if(dataSnapshot.exists()){
+                if (dataSnapshot.exists()) {
                     //User already exits
                 } else {
                     appDatabase.child("users").child(uniqueId).setValue("Grinch");      //Change Grinch to username requested from user - Optional, not critical
@@ -115,48 +151,46 @@ public class Main2Activity extends AppCompatActivity implements View.OnClickList
             @Override
             public void onClick(View v) {
 
-                new FragNewListNameDialog().show(getSupportFragmentManager(),null);
+                new FragNewListNameDialog().show(getSupportFragmentManager(), null);
             }
         });
 
         editListBtn.setOnClickListener(this);
-
     }
 
     @Override
-    public void onClick(View v) {
-        state = 2;
-        Intent myIntent = new Intent(Main2Activity.this, EditListActivity.class);
-        myIntent.putExtra("UID", uniqueId);
-        myIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        Main2Activity.this.startActivity(myIntent);                                 //Load edit list activity here*/
-    }
+        public void onClick (View v){
+            state = 2;
+            Intent myIntent = new Intent(Main2Activity.this, EditListActivity.class);
+            myIntent.putExtra("UID", uniqueId);
+            myIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            Main2Activity.this.startActivity(myIntent);                                 //Load edit list activity here*/
+        }
 
-    @Override
-    public void onListNameApproved(final String name) {
+        @Override
+        public void onListNameApproved ( final String name){
 
-        appDatabase.child(LISTS).child(uniqueId).addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                if(!dataSnapshot.hasChild(name)){
-                    Intent myIntent = new Intent(Main2Activity.this, NewListActivity.class);
-                    myIntent.putExtra("LIST_NAME", name);                       //pass parameters for the new activity
-                    myIntent.putExtra("FIREBASE_URL", FIREBASE_URL);            //
-                    myIntent.putExtra("UID", uniqueId);                         //
-                    myIntent.putExtra("STATE", state);                          //
-                    Main2Activity.this.startActivity(myIntent);                       //Load new list activity here*/
+            appDatabase.child(LISTS).child(uniqueId).addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    if (!dataSnapshot.hasChild(name)) {
+                        Intent myIntent = new Intent(Main2Activity.this, NewListActivity.class);
+                        myIntent.putExtra("LIST_NAME", name);                       //pass parameters for the new activity
+                        myIntent.putExtra("FIREBASE_URL", FIREBASE_URL);            //
+                        myIntent.putExtra("UID", uniqueId);                         //
+                        myIntent.putExtra("STATE", state);                          //
+                        Main2Activity.this.startActivity(myIntent);                       //Load new list activity here*/
+                    } else
+                        Toast.makeText(getApplicationContext(), "Another list with the same name already exists", Toast.LENGTH_LONG).show();
                 }
-                else
-                    Toast.makeText(getApplicationContext(), "Another list with the same name already exists",Toast.LENGTH_LONG).show();
-            }
 
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
 
-            }
-        });
+                }
+            });
 
-    }
+        }
 
     /**Check permission in order to obtain unique user ID*/
     @Override
@@ -166,9 +200,11 @@ public class Main2Activity extends AppCompatActivity implements View.OnClickList
             case MY_PERMISSIONS_REQUEST_READ_PHONE_STATE:
                 if (ContextCompat.checkSelfPermission(Main2Activity.this, Manifest.permission.READ_PHONE_STATE) == PackageManager.PERMISSION_GRANTED) {
                     Context AppContext = getApplicationContext();
+                    this.permissions=true;
                     TelephonyManager tMgr = (TelephonyManager)AppContext.getSystemService(Context.TELEPHONY_SERVICE);
                     uniqueId = tMgr.getDeviceId();
                     smsReceiver = new SmsBroadcastReceiver(uniqueId);                                           //Creating new BroadcastReceiver
+                    startApp();
                 }
                 break;
             case MY_PERMISSIONS_REQUEST_READ_SMS:
